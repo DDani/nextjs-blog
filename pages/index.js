e7062c7bb4f2d6da18fb12a3467a432c482620aa
Dani <danicasro>
2021-08-30 00:32:26 +0200

    -Implemented all tutorial files. Let's see how it get's deployed to Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Date from '../components/date'
-import Link from 'next/Link'
+import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
